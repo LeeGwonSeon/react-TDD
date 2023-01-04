51d2dc16bd5ad85f7391043aef3388aa722ebeac
Logan Lee <verygoody2k@hotmail.com>
2023-01-04 11:26:20 +0900

    React A-Z_on/off 버튼 클릭 시 버튼 disabled

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
         <button
           data-testid="minus-button"
+          disabled={disabled}
           onClick={() => setCounter((prev) => prev - 1)}
         >
           -
         </button>
         <button
           data-testid="plus-button"
+          disabled={disabled}
           onClick={() => setCounter((prev) => prev + 1)}
         >
           +
@@ -24,6 +27,9 @@ function App() {
           <button
             style={{ backgroundColor: "blue" }}
             data-testid="on/off-button"
+            onClick={() =>{
+              setDisabled((prev)=> !prev)
+            }}
           >
             on/off
           </button>
